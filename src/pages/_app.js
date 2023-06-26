0c97d0b165c0c92607e8af02d4628db34135a944
Samir Alibabic <samir.alibabic@valtech.de>
2023-06-26 18:06:14 +0200

    Store google fonts locally for performance and security.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,22 @@
 import Theme from '../styles/theme';
 import { Analytics } from '@vercel/analytics/react';
+import { Space_Grotesk } from 'next/font/google'
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300', '400', '500', '600', '700'],
+})
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
+      <style jsx global>{`
+        html {
+          font-family: ${spaceGrotesk.style.fontFamily};
+        }
+      `}</style>
         <Component {...pageProps} />
         <Analytics />
       </Theme>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,9 +30,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='en-GB'>
-        <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />

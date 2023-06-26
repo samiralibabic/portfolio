4a44db6bde9394864139a698a806d19e5a2b1f61
Samir Alibabic <samir.alibabic@valtech.de>
2023-06-26 13:02:21 +0200

    Added links to github, blog and internal tech section to hero section text.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,17 +6,19 @@ import { LeftSection } from './HeroStyles';
 import Link from 'next/link';
 
 const Hero = (props) => (
-    <LeftSection>
-      <SectionTitle main center>
-        Discover the transformative power of captivating digital experiences.
-      </SectionTitle>
-      <SectionText>
-        Welcome to my developer portfolio, where I combine a passion for coding,
-        a creative mindset, and the utilization of emerging technologies to deliver
-        innovative and impactful solutions to challenges.
-      </SectionText>
-      <Button onClick={() => window.location = '#contact'}>Contact me</Button>
-    </LeftSection>
+  <LeftSection>
+    <SectionTitle main center>
+      Discover the transformative power of captivating digital experiences.
+    </SectionTitle>
+    <SectionText>
+      Welcome to my developer portfolio, where I combine a passion for <Link href="https://github.com/samiralibabic" 
+      style={{ color: "white" }} target='_blank'> coding</Link>, a <Link href="https://samiralibabic.hashnode.dev/" 
+      style={{ color: "white" }} target='_blank'>creative</Link> mindset, and the utilization of emerging <Link href='#tech'
+      style={{ color: "white" }} scroll={false}>technologies</Link> to deliver
+      innovative and impactful solutions to challenges.
+    </SectionText>
+    <Button onClick={() => window.location = '#contact'}>Contact me</Button>
+  </LeftSection>
 );
 
 export default Hero;

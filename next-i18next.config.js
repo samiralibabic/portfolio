908ad1b0d449943c5b427b5d57520dd9bc3509b7
Samir Alibabic <samir.alibabic@gmail.com>
2025-03-05 16:02:44 +0100

    Disable automatic locale detection in next-i18next configuration

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
-    localeDetection: true,
+    localeDetection: false,
     domains: [
       {
         domain: 'www.samiralibabic.com',
